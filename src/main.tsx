08b63f0ce210fbb2b7ac40ef295c0268ffb295b1
lgklsv <kolosov20011@gmail.com>
2023-03-14 22:52:33 +0300

    fix: fix routing integration with tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
