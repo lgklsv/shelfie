937a1425429a4c138bfe767278b1d5464cd4b7e8
lgklsv <kolosov20011@gmail.com>
2023-03-29 12:31:18 +0300

    refactor: change footer to functional component

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -2,35 +2,33 @@ import React from 'react';
 
 import styles from './Footer.module.scss';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.footer__container}>
-          <div>
-            shelfie engineered by{' '}
-            <a
-              className={styles.footer__link}
-              href="https://github.com/lgklsv"
-              target="_blank"
-              rel="noreferrer"
-            >
-              lgklsv
-            </a>
-          </div>
-          <div className={styles.footer__copyright}>
-            <a
-              className={styles.footer__link_rs}
-              href="https://rs.school/react/"
-              target="_blank"
-              rel="noreferrer"
-            />
-            © 2023 shelfie. All rights reserved.
-          </div>
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div>
+          shelfie engineered by{' '}
+          <a
+            className={styles.footer__link}
+            href="https://github.com/lgklsv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            lgklsv
+          </a>
         </div>
-      </footer>
-    );
-  }
-}
+        <div className={styles.footer__copyright}>
+          <a
+            className={styles.footer__link_rs}
+            href="https://rs.school/react/"
+            target="_blank"
+            rel="noreferrer"
+          />
+          © 2023 shelfie. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
