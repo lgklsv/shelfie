da7c39fb3d7944399d89f74fd423a1e32205a9ed
lgklsv <kolosov20011@gmail.com>
2023-03-30 10:10:57 +0300

    refactor: rewrite from to FC and hooks retaining fucntionality

--- src/pages/suggest/form/BookFormSection.tsx
+++ src/pages/suggest/form/BookFormSection.tsx
@@ -12,57 +12,60 @@ import { bookCategories } from 'shared/model/book-categories';
 import { validateBookForm } from './handlers/validateBookForm';
 import styles from './BookFormSection.module.scss';
 
-class BookFormSection extends React.Component<BooksFromProps, BookFormState> {
-  private formRef = React.createRef<HTMLFormElement>();
-  private titleInputRef = React.createRef<HTMLInputElement>();
-  private authorInputRef = React.createRef<HTMLInputElement>();
-  private dateInputRef = React.createRef<HTMLInputElement>();
-  private categoryInputRef = React.createRef<HTMLSelectElement>();
-  private typeEBookInputRef = React.createRef<HTMLInputElement>();
-  private typePrintedInputRef = React.createRef<HTMLInputElement>();
-  private imageInputRef = React.createRef<HTMLInputElement>();
-  private agreementInputRef = React.createRef<HTMLInputElement>();
-  constructor(props: BooksFromProps) {
-    super(props);
-    this.state = {
-      titleMessage: '',
-      authorMessage: '',
-      dateMessage: '',
-      selectMessage: '',
-      radioMessage: '',
-      imageMessage: '',
-      checkboxMessage: '',
-      popup: {
-        isVisible: false,
-        type: 'error',
-        message: '',
-      },
-    };
+type PopupState = {
+  isVisible: boolean;
+  type: 'success' | 'error';
+  message: string;
+};
 
-    this.handlePopUpunmount = this.handlePopUpunmount.bind(this);
-    this.submitHandler = this.submitHandler.bind(this);
-  }
+type BooksFromProps = {
+  addBook: (book: SuggestedBook) => void;
+};
 
-  handlePopUpunmount() {
-    this.setState(() => ({
-      popup: {
-        isVisible: false,
-        type: 'error',
-        message: 'Error',
-      },
-    }));
-  }
+const BookFormSection: React.FC<BooksFromProps> = ({ addBook }) => {
+  const formRef = React.useRef<HTMLFormElement>(null);
+  const titleInputRef = React.useRef<HTMLInputElement>(null);
+  const authorInputRef = React.useRef<HTMLInputElement>(null);
+  const dateInputRef = React.useRef<HTMLInputElement>(null);
+  const categoryInputRef = React.useRef<HTMLSelectElement>(null);
+  const typeEBookInputRef = React.useRef<HTMLInputElement>(null);
+  const typePrintedInputRef = React.useRef<HTMLInputElement>(null);
+  const imageInputRef = React.useRef<HTMLInputElement>(null);
+  const agreementInputRef = React.useRef<HTMLInputElement>(null);
 
-  submitHandler(event: React.FormEvent<HTMLFormElement>) {
+  const [formState, setFormState] = React.useState({
+    titleMessage: '',
+    authorMessage: '',
+    dateMessage: '',
+    selectMessage: '',
+    radioMessage: '',
+    imageMessage: '',
+    checkboxMessage: '',
+  });
+  const [popupState, setPopupState] = React.useState<PopupState>({
+    isVisible: false,
+    type: 'error',
+    message: '',
+  });
+
+  const handlePopUpunmount = () => {
+    setPopupState({
+      isVisible: false,
+      type: 'error',
+      message: 'Error',
+    });
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const enteredTitle = this.titleInputRef.current;
-    const enteredAuthor = this.authorInputRef.current;
-    const enteredDate = this.dateInputRef.current;
-    const enteredCategory = this.categoryInputRef.current;
-    const enteredTypeEbook = this.typeEBookInputRef.current;
-    const enteredTypePrited = this.typePrintedInputRef.current;
-    const enteredImage = this.imageInputRef.current;
-    const enteredAgreement = this.agreementInputRef.current;
+    const enteredTitle = titleInputRef.current;
+    const enteredAuthor = authorInputRef.current;
+    const enteredDate = dateInputRef.current;
+    const enteredCategory = categoryInputRef.current;
+    const enteredTypeEbook = typeEBookInputRef.current;
+    const enteredTypePrited = typePrintedInputRef.current;
+    const enteredImage = imageInputRef.current;
+    const enteredAgreement = agreementInputRef.current;
 
     const { isValid, report } = validateBookForm(
       enteredTitle,
@@ -87,7 +90,7 @@ class BookFormSection extends React.Component<BooksFromProps, BookFormState> {
       },
     };
 
-    this.setState(() => ({
+    setFormState({
       titleMessage: report.title.message,
       authorMessage: report.author.message,
       dateMessage: report.date.message,
@@ -95,131 +98,125 @@ class BookFormSection extends React.Component<BooksFromProps, BookFormState> {
       radioMessage: report.radio.message,
       imageMessage: report.image.message,
       checkboxMessage: report.checkbox.message,
-    }));
+    });
 
     if (!isValid) {
-      this.setState(() => ({
-        popup: {
-          isVisible: true,
-          type: 'error',
-          message: 'All form fields are required !',
-        },
-      }));
+      setPopupState({
+        isVisible: true,
+        type: 'error',
+        message: 'All form fields are required !',
+      });
       return;
     }
 
-    this.formRef.current?.reset();
-    this.props.addBook(newBook);
+    formRef.current?.reset();
+    addBook(newBook);
 
-    this.setState(() => ({
-      popup: {
-        isVisible: true,
-        type: 'success',
-        message: 'Your suggestion successfully added! 🎉',
-      },
-    }));
-  }
+    setPopupState({
+      isVisible: true,
+      type: 'success',
+      message: 'Your suggestion successfully added! 🎉',
+    });
+  };
 
-  render() {
-    return (
-      <section className="container">
-        <div className={styles.form_container}>
-          <form
-            ref={this.formRef}
-            onSubmit={this.submitHandler}
-            className={`${styles.form} container_sm`}
-            noValidate
-          >
-            <h3 className={styles.form__title}>Book details</h3>
-            <TextLikeInput
-              innerRef={this.titleInputRef}
-              type="text"
-              name="title"
-              id="title"
-              message={this.state.titleMessage}
-              placeholder="Title"
-            />
-            <TextLikeInput
-              innerRef={this.authorInputRef}
-              type="text"
-              name="author"
-              id="author"
-              message={this.state.authorMessage}
-              placeholder="Author"
-            />
-            <div className={styles.form__split}>
-              <TextLikeInput
-                innerRef={this.dateInputRef}
-                type="date"
-                name="published"
-                id="published"
-                message={this.state.dateMessage}
-                placeholder="Published date"
-              />
-              <SelectInput
-                innerRef={this.categoryInputRef}
-                name="category"
-                id="category"
-                defaultText="Choose category"
-                message={this.state.selectMessage}
-                options={bookCategories}
-              />
-            </div>
-            <div className={styles.form__split_half}>
-              <div className={styles.form__radio}>
-                <div className={styles.form__radio__group}>
-                  <RadioInput
-                    innerRef={this.typeEBookInputRef}
-                    name="book-type"
-                    id="e-book"
-                    text="eBook"
-                  />
-                  <RadioInput
-                    innerRef={this.typePrintedInputRef}
-                    name="book-type"
-                    id="print"
-                    text="Printed Book"
-                  />
-                </div>
-                <span className={styles.error__message}>
-                  {this.state.radioMessage}
-                </span>
-              </div>
-              <ImageInput
-                innerRef={this.imageInputRef}
-                name="image"
-                id="image"
-                text="Choose image"
-                message={this.state.imageMessage}
-              />
-            </div>
-            <div className={styles.form__checkboxarea}>
-              <CheckboxInput
-                innerRef={this.agreementInputRef}
-                name="agreement"
-                id="agreement"
-                text="I have read and agree to the Terms of Service"
-                message={this.state.checkboxMessage}
-              />
-            </div>
-            <button
-              type="submit"
-              className={`btn btn-primary ${styles.form__btn}`}
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-        {this.state.popup.isVisible && (
-          <PopupNotification
-            type={this.state.popup.type}
-            message={this.state.popup.message}
-            unmountMe={this.handlePopUpunmount}
+  return (
+    <section className="container">
+      <div className={styles.form_container}>
+        <form
+          ref={formRef}
+          onSubmit={submitHandler}
+          className={`${styles.form} container_sm`}
+          noValidate
+        >
+          <h3 className={styles.form__title}>Book details</h3>
+          <TextLikeInput
+            innerRef={titleInputRef}
+            type="text"
+            name="title"
+            id="title"
+            message={formState.titleMessage}
+            placeholder="Title"
           />
-        )}
-      </section>
-    );
-  }
-}
+          <TextLikeInput
+            innerRef={authorInputRef}
+            type="text"
+            name="author"
+            id="author"
+            message={formState.authorMessage}
+            placeholder="Author"
+          />
+          <div className={styles.form__split}>
+            <TextLikeInput
+              innerRef={dateInputRef}
+              type="date"
+              name="published"
+              id="published"
+              message={formState.dateMessage}
+              placeholder="Published date"
+            />
+            <SelectInput
+              innerRef={categoryInputRef}
+              name="category"
+              id="category"
+              defaultText="Choose category"
+              message={formState.selectMessage}
+              options={bookCategories}
+            />
+          </div>
+          <div className={styles.form__split_half}>
+            <div className={styles.form__radio}>
+              <div className={styles.form__radio__group}>
+                <RadioInput
+                  innerRef={typeEBookInputRef}
+                  name="book-type"
+                  id="e-book"
+                  text="eBook"
+                />
+                <RadioInput
+                  innerRef={typePrintedInputRef}
+                  name="book-type"
+                  id="print"
+                  text="Printed Book"
+                />
+              </div>
+              <span className={styles.error__message}>
+                {formState.radioMessage}
+              </span>
+            </div>
+            <ImageInput
+              innerRef={imageInputRef}
+              name="image"
+              id="image"
+              text="Choose image"
+              message={formState.imageMessage}
+            />
+          </div>
+          <div className={styles.form__checkboxarea}>
+            <CheckboxInput
+              innerRef={agreementInputRef}
+              name="agreement"
+              id="agreement"
+              text="I have read and agree to the Terms of Service"
+              message={formState.checkboxMessage}
+            />
+          </div>
+          <button
+            type="submit"
+            className={`btn btn-primary ${styles.form__btn}`}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      {popupState.isVisible && (
+        <PopupNotification
+          type={popupState.type}
+          message={popupState.message}
+          unmountMe={handlePopUpunmount}
+        />
+      )}
+    </section>
+  );
+};
 
 export default BookFormSection;
