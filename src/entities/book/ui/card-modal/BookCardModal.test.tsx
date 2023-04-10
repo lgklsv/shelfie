65a5fa5307c2580c6f4989bdc3c11048be3c0092
lgklsv <kolosov20011@gmail.com>
2023-04-10 21:07:07 +0300

    fix: remove unused import

--- src/entities/book/ui/card-modal/BookCardModal.test.tsx
+++ src/entities/book/ui/card-modal/BookCardModal.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import BookCardModal from './BookCardModal';
