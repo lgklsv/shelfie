82f5e623f9153f361c918a2b7f8594def5b9e358
lgklsv <kolosov20011@gmail.com>
2023-04-14 00:26:45 +0300

    refactor: change search bar tests to support redux store and new changes

--- src/features/searchbar/search/ui/Search.test.tsx
+++ src/features/searchbar/search/ui/Search.test.tsx
@@ -1,12 +1,17 @@
+import { Provider } from 'react-redux';
 import { describe, it } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 
+import { store } from 'app/store';
 import Search from './Search';
 
 describe('searchbar', () => {
   it('should change search input value on user input', () => {
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>
+    );
     const searchInput = screen.getByRole('searchbox') as HTMLInputElement;
     const testValue = 'some input';
 
@@ -14,14 +19,21 @@ describe('searchbar', () => {
     expect(searchInput.value).toBe(testValue);
   });
   it('should not show clear btn if input is empty', () => {
-    localStorage.clear();
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>
+    );
     const clearIcon = screen.queryByTestId('clear-btn');
 
     expect(clearIcon).not.toBeInTheDocument();
   });
   it('should clear search input on clicking cross btn and hide clear btn', () => {
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>
+    );
     const searchInput = screen.getByRole('searchbox') as HTMLInputElement;
     const testValue = 'some input';
 
@@ -31,29 +43,5 @@ describe('searchbar', () => {
     fireEvent.click(clearIcon);
     expect(searchInput.value).toBe('');
     expect(clearIcon).not.toBeInTheDocument();
-  });
-  it('should save search value in localstorage on unmount and restore on mount', () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Search />
-      </MemoryRouter>
-    );
-    const searchInput = screen.getByRole('searchbox') as HTMLInputElement;
-    const testValue = 'some input';
-
-    fireEvent.change(searchInput, { target: { value: testValue } });
-
-    render(
-      <MemoryRouter initialEntries={['/about']}>
-        <Search />
-      </MemoryRouter>
-    );
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Search />
-      </MemoryRouter>
-    );
-
-    expect(searchInput.value).toBe(testValue);
   });
 });
--- src/pages/home/welcome/WelcomeSection.test.tsx
+++ src/pages/home/welcome/WelcomeSection.test.tsx
@@ -1,10 +1,17 @@
+import { Provider } from 'react-redux';
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+
+import { store } from 'app/store';
 import Welcome from './WelcomeSection';
 
 describe('Welcome section', () => {
   it('should have search input element', () => {
-    render(<Welcome />);
+    render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    );
     const searchInput = screen.getByRole('searchbox');
     expect(searchInput).toBeInTheDocument();
   });
