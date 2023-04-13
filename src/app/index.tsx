42e14957ac80cb0545ccf997ccb11af77c4a8733
lgklsv <kolosov20011@gmail.com>
2023-04-13 17:24:24 +0300

    refactor: remove react-query leftovers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Routing from 'pages';
 import { store } from './store';
 import { appTitle } from './config';
 import './index.scss';
 
-const queryClient = new QueryClient();
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient} contextSharing>
-        <Helmet defaultTitle={appTitle} titleTemplate={`%s | ${appTitle}`} />
-        <Routing />
-      </QueryClientProvider>
+      <Helmet defaultTitle={appTitle} titleTemplate={`%s | ${appTitle}`} />
+      <Routing />
     </Provider>
   );
 };
